92c0a1922dec292f7234624f85d89468b06bf9f3
Siddharth Tiwari <siddy1101@gmail.com>
2018-06-01 19:12:53 +0530

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -14,54 +14,54 @@ let win
 
 
 function createWindow () {
-	win = new BrowserWindow({
-		'width': 325,
-		'height': 295,
-		'max-width': 325,
-		'max-height': 324,
-		'min-width': 325,
-		'min-height': 280
-	});
+        win = new BrowserWindow({
+                'width': 325,
+                'height': 408,
+                'max-width': 325,
+                'max-height': 450,
+                'min-width': 325,
+                'min-height': 250
+        });
 
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, './index.html'),
-		protocol: 'file:',
-		slashes: true
-	}))
+        win.loadURL(url.format({
+                pathname: path.join(__dirname, './index.html'),
+                protocol: 'file:',
+                slashes: true
+        }))
 
-	win.on('closed',() => {
-		win = null;
-	})
-//	win.openDevTools()
+        win.on('closed',() => {
+                win = null;
+        })
+//      win.openDevTools()
 
 }
 app.on('ready', createWindow)
 
 
 app.on('window-all-closed', function() {
-	if (process.platform != 'darwin') {
-		app.quit();
-	}
+        if (process.platform != 'darwin') {
+                app.quit();
+        }
 });
 app.on('activate', () => {
-	if (win == null){
-		createWindow()
-	}
+        if (win == null){
+                createWindow()
+        }
 })
 
 //app.on('ready', function() {
-//	mainWindow = new BrowserWindow({
-//		'width': 325,
-//		'height': 324,
-//		'max-width': 325,
-//		'max-height': 324,
-//		'min-width': 325,
-//		'min-height': 324
-//	});
+//      mainWindow = new BrowserWindow({
+//              'width': 325,
+//              'height': 324,
+//              'max-width': 325,
+//              'max-height': 324,
+//              'min-width': 325,
+//              'min-height': 324
+//      });
 
-//	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+//      mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-//	mainWindow.on('closed', function() {
-//		mainWindow = null;
-//	});
+//      mainWindow.on('closed', function() {
+//              mainWindow = null;
+//      });
 //});
